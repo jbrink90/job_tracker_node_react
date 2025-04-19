ffdff7955c047c72f88f368298538f55be4a177d
jbrink90 <jordanabrinkman@gmail.com>
2025-04-19 01:49:03 -0400

    Dockerfile test. Drop shadows. Border radius. Blank Login Page. Navlinks.

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,18 +1,25 @@
-import * as React from "react";
+import React, { FC } from "react";
+import { NavLink } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import "./index.css";
 
-const NavBar = () => {
-  const [anchorElement, setAnchorElement] = React.useState<null | HTMLElement>(null);
+interface NavBarProps {
+  isUserLoggedIn: boolean;
+}
+
+const NavBar: FC<NavBarProps> = ({ isUserLoggedIn }) => {
+  const [anchorElement, setAnchorElement] = React.useState<null | HTMLElement>(
+    null
+  );
 
   const open = Boolean(anchorElement);
 
   const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorElement(event.currentTarget);
   };
-  
+
   const closeMenu = () => {
     setAnchorElement(null);
   };
@@ -21,24 +28,63 @@ const NavBar = () => {
     <>
       <div className="navBar_main">
         <div className="navBar_userDiv">
-            <Button
-              id="basic-button"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={openMenu}
-            >
-              Welcome Jordan
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorElement}
-              open={open}
-              onClose={closeMenu}
-            >
-              <MenuItem onClick={closeMenu}>My account</MenuItem>
-              <MenuItem onClick={closeMenu}>Logout</MenuItem>
-            </Menu>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={openMenu}
+          >
+            {isUserLoggedIn ? "Welcome Jordan" : "Welcome Guest"}
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorElement}
+            open={open}
+            onClose={closeMenu}
+          >
+            {isUserLoggedIn
+              ? [
+                  <MenuItem onClick={closeMenu} key="account">
+                    <NavLink
+                      to="/account"
+                      style={{ textDecoration: "none" }}
+                      onClick={closeMenu}
+                    >
+                      My account
+                    </NavLink>
+                  </MenuItem>,
+                  <MenuItem onClick={closeMenu} key="logout">
+                    <NavLink
+                      to="/logout"
+                      style={{ textDecoration: "none" }}
+                      onClick={closeMenu}
+                    >
+                      Logout
+                    </NavLink>
+                  </MenuItem>,
+                ]
+              : [
+                  <MenuItem key="login">
+                    <NavLink
+                      to="/login"
+                      style={{ textDecoration: "none" }}
+                      onClick={closeMenu}
+                    >
+                      Log In
+                    </NavLink>
+                  </MenuItem>,
+                  <MenuItem onClick={closeMenu} key="create">
+                    <NavLink
+                      to="/create"
+                      style={{ textDecoration: "none" }}
+                      onClick={closeMenu}
+                    >
+                      Create Account
+                    </NavLink>
+                  </MenuItem>,
+                ]}
+          </Menu>
         </div>
       </div>
     </>
