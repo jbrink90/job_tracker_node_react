0895c1bdfe2d2f8651c72b5bbd4ae3d501c1239c
jbrink90 <jordanabrinkman@gmail.com>
2025-12-05 02:36:53 -0500

    Testing placing supabase name on header.

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -4,3 +4,24 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON
 );
+
+export async function getCurrentUser() {
+  const { data, error } = await supabase.auth.getUser();
+  if (error) console.log(error);
+  return data.user;
+}
+
+export async function getUserEmail() {
+  const { data, error } = await supabase.auth.getUser();
+  if (error) console.log(error);
+  return data.user?.email;
+}
+
+export async function getUserEmailSplit() {
+  // Return a capitalized version of the part of the email before the '@' symbol
+  const { data, error } = await supabase.auth.getUser();
+  if (error) console.log(error);
+  const email = data.user?.email?.split('@')[0];
+  if (!email) return null;
+  return email.charAt(0).toUpperCase() + email.slice(1);
+}
