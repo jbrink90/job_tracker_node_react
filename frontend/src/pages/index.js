462460b8bccc31261162b18363e635638ee43b6e
jbrink90 <jordanabrinkman@gmail.com>
2025-12-15 23:21:52 -0500

    Removed extra compiled js files. Modified dashboard header css.

--- frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home";
-export { default as SimpleDashboard } from "./SimpleDashboard";
-export { default as Map } from "./Map";
-export { default as Login } from "./Login";
-export { default as Logout } from "./Logout";
-export { default as AuthCallback } from "./AuthCallback";
-export { default as Account } from "./Account";
