ab73d53c70d8b3c4ee4b6cfcc40beebdf5777db6
jbrink90 <jordanabrinkman@gmail.com>
2026-01-09 21:12:39 -0500

    Partial refactor of endpoints to use next() and better error handling.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import jobRoutes from "./routes/jobs";
 import dotenv from "dotenv";
-import { createDatabase } from "./routes/jobs";
+import { createDatabase, addDemoData } from "./routes/jobs";
 
 dotenv.config();
 
@@ -15,10 +15,11 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use("/jobs", jobRoutes);
+
 createDatabase();
+addDemoData();
 
 if (process.env.NODE_ENV !== "lambda" && process.env.NODE_ENV !== "production") {
   const port = process.env.API_PORT || 4444;
