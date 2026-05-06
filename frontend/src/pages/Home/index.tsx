46f99373c63c1a294e3c4905d9a27fbce08c6347
Theconsciousness <jordanabrinkman@gmail.com>
2026-05-05 20:17:05 -0400

    Added direct link to dashboard to bypass login.

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -129,7 +129,7 @@ export default function Home() {
                 <Button
                   variant="contained"
                   disableElevation
-                  onClick={() => navigate("/login")}
+                  onClick={() => navigate("/dashboard")}
                 >
                   Get Started
                 </Button>
